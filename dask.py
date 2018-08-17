9ac732ba3130963cb3f7a435b8f0e213638adcdf
Jay Shah <jayshah5696@gmail.com>
2018-08-16 23:09:46 -0700

    update

--- dask.py
+++ dask.py
@@ -22,6 +22,14 @@ client = Client() # start a local Dask client
 
 # You can navigate to http://localhost:8787/status to see the diagnostic dashboard if you have Bokeh installed.
 
+# Instead of creatinf direct client, you can form your own cluster and than build client on top of thatself.
+
+from dask.distributed import Client, LocalCluster
+cluster = LocalCluster()
+client = Client(cluster)
+
+# This will allow you to control your clusters properties.
+
 
 #The next step will be to instantiate dask joblib in the
 # backend. You need to import parallel_backend from sklearn # joblib like I have shown below.
@@ -33,3 +41,17 @@ with parallel_backend('dask'):
     # Your normal scikit-learn code here
      from sklearn.ensemble import RandomForestClassifier
      model = RandomForestClassifier()
+
+
+# I want to parallize the custom workflows
+# This can be used with the sciki learn pipeline.
+
+# Let's say I have a function that do some process on the data.
+# And I want to parallize the process using dask_ml
+
+def process(data):
+
+    return something
+
+# Normal process
+results = [processs(x) for x in inputs]
